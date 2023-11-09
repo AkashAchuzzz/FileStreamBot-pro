25a812cc765e5bd52b48ae71e7510dea1185d278
Akash Krishna <99610857+AkashAchuzzz@users.noreply.github.com>
2023-11-09 14:38:45 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', "16970144"))
+    API_ID = int(getenv('API_ID', 16970144
     API_HASH = str(getenv('API_HASH', "9aaa8123d070b0507ebb24dac8443857"))
     BOT_TOKEN = str(getenv('BOT_TOKEN', "5479481991:AAHtca0jxG7Uqf5aeQTrpcd9Cb3TPC00QEQ"))
     name = str(getenv('name', 'filetolinkbot'))
@@ -19,7 +19,7 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", 16970144).split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", 969730932).split)) 
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', "AkashAchuzzz"))
